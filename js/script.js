b36ca0bf4cbed6ef8cf53d46f8fa24e13be7422e
Brenna Silva Carvalho <brenna.silva.carvalho@outlook.com.br>
2020-10-24 19:33:52 -0300

    versão com css

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const app = new Vue({
+  el: "#app",
+  vuetify: new Vuetify(),
+  data() {
+    return {
+      errors: [],
+      theme:{
+      dark: false,
+      darkClass: "glint",
+      darkBackgroundColor: "bg-glint",
+      darkCard: "card-glint",
+      darkInput: "input-glint",
+      },
+      form:{
+        nome: null,
+        sobrenome: null,
+        email: null,
+        senha: null,
+      }
+  };
+  },
+  methods: {
+    setDark() {
+      this.theme.dark = !this.theme.dark;
+      if (this.theme.dark) {
+        this.theme.darkClass = "glint-dark";
+        this.theme.darkBackgroundColor = "bg-glint-dark";
+        this.theme.darkCard = "card-glint-dark";
+        this.theme.darkInput = "input-glint-dark";
+      } else {
+        this.theme.darkClass = "glint";
+        this.theme.darkBackgroundColor = "bg-glint";
+        this.theme.darkCard = "card-glint";
+        this.theme.darkInput = "input-glint";
+      }
+    },
+    checkForm: function (e) {
+      if (this.form.nome) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.form.name) {
+        this.errors.push("O nome é obrigatório.");
+      }
+
+      e.preventDefault();
+    },
+  },
+});
